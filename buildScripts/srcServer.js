a972874b4e18ef01d66df476f779f0993ead6d32
Zach Steffens <zachary.steffens@weyerhaeuser.com>
2018-06-15 08:58:58 -0600

    work in progress

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+//this script leverages the Babel transpliler
+
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 12000;
+const app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
